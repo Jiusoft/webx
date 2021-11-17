e611bdf2aa77447b19f0811e59af70213edfe4b8
Jothin kumar <bjothinphysics@gmail.com>
2021-11-17 13:17:00 +0530

    Added GUI for downloads

--- scripts/download.py
+++ scripts/download.py
@@ -1,6 +1,19 @@
 """
 Original repository link: https://github.com/Jiusoft/fax-browser
 """
+import tkinter as tk
+
+
 def download_file(item):
-    item.accept()
+    root = tk.Tk()
+
+    def save():
+        item.accept()
+        root.destroy()
+
+    tk.Label(master=root,
+             text=f'What would you like to do with {item.suggestedFileName()} ({item.totalBytes()} Bytes)').pack()
+    tk.Button(master=root, text='Save', command=save).pack()
+    tk.Button(master=root, text='Cancel', command=root.destroy).pack()
+    root.mainloop()
 # Enhancements coming soon...
