7133b8f91e1b9aac77895cc81efae41bbba3013e
Jothin kumar <bjothinphysics@gmail.com>
2021-11-17 13:19:26 +0530

    Add title in download GUI

--- scripts/download.py
+++ scripts/download.py
@@ -6,13 +6,15 @@ import tkinter as tk
 
 def download_file(item):
     root = tk.Tk()
+    filename = item.suggestedFileName()
+    root.wm_title(f'{filename} download - Jiusoft fax browser')
 
     def save():
         item.accept()
         root.destroy()
 
     tk.Label(master=root,
-             text=f'What would you like to do with {item.suggestedFileName()} ({item.totalBytes()} Bytes)').pack()
+             text=f'What would you like to do with {filename} ({item.totalBytes()} Bytes)').pack()
     tk.Button(master=root, text='Save', command=save).pack()
     tk.Button(master=root, text='Cancel', command=root.destroy).pack()
     root.mainloop()
