5e6deb8d4209ab3fb6142214d7d2623c51ae41a3
Jothin kumar <bjothinphysics@gmail.com>
2021-12-24 16:25:04 +0530

    Better GUI
    Signed-off-by: Jothin kumar <bjothinphysics@gmail.com>

--- scripts/fax-launcher.py
+++ scripts/fax-launcher.py
@@ -6,8 +6,10 @@ root = tk.Tk()
 root.overrideredirect(True)
 root.geometry(f'400x200+{int(root.winfo_screenwidth() / 2) - 200}+{int(root.winfo_screenheight() / 2 - 100)}')
 root.configure(background='green')
-progress_label = tk.Label(master=root, text='', font=('Ariel', 20), bg='green')
-progress_label.pack()
+tk.Label(master=root, text='FAX browser', font=('Ariel', 30), bg='green').pack()
+tk.Label(master=root, text='By Jiusoft', font=('Ariel', 20), bg='green').pack()
+progress_label = tk.Label(master=root, text='', font=('Ariel', 15), bg='green')
+progress_label.pack(side=tk.BOTTOM)
 
 
 def check_for_updates():
