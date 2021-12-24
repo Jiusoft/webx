081067d272cc9ec9ec9cb67f5518f0e323303af5
Jothin kumar <bjothinphysics@gmail.com>
2021-12-24 16:41:24 +0530

    Implement latest version check.
    Signed-off-by: Jothin kumar <bjothinphysics@gmail.com>

--- scripts/fax-launcher.py
+++ scripts/fax-launcher.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from time import sleep
 from threading import Thread
+from requests import get
+from datetime import datetime
+
+version = '1.0.0'
 
 root = tk.Tk()
 root.overrideredirect(True)
@@ -12,10 +16,14 @@ progress_label = tk.Label(master=root, text='', font=('Ariel', 15), bg='green')
 progress_label.pack(side=tk.BOTTOM)
 
 
-def check_for_updates():
-    progress_label['text'] = 'Checking for updates'
+def log(message):
+    with open('logs.txt', 'a+') as log_file:
+        log_file.write(f'{message}\n')
 
+
+def check_for_updates():
     def progress_update_command():
+        progress_label['text'] = 'Checking for updates'
         while True:
             sleep(0.5)
             if progress_label['text'] == 'Checking for updates':
@@ -30,6 +38,10 @@ def check_for_updates():
                 break
 
     Thread(target=progress_update_command).start()
+    latest_version = get('https://cdn.jiu-soft.com/fax-browser/latest-version.txt').text.strip('\n')
+    now = datetime.now()
+    log(f'[{now.year}:{now.month}:{now.day}:{now.hour}:{now.minute}:{now.second}]: lv - {latest_version}')
+    return latest_version == version
 
 
 Thread(target=check_for_updates).start()
